c93347ac4221c4f3ebf449dc38f78c0111486baf
kuknion2669-sketch <kuknion2669-sketch@users.noreply.github.com>
2026-07-14 08:43:19 +0000

    fix: update haproxy_sock path to match Debian 11 HAProxy default

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     def __init__(self):
         self.root = '/root/tcp-panel-v2'
         self.db_file = '/root/traffic.db'         # Same DB as v13 - no data disruption
-        self.haproxy_sock = '/run/haproxy.sock'
+        self.haproxy_sock = '/run/haproxy/admin.sock'
         self.haproxy_cfg = '/etc/haproxy/haproxy.cfg'
         self.haproxy_pid = '/run/haproxy.pid'
         self.default_port = '8081'                 # Different port from v13 (8080)
